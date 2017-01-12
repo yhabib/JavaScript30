e099c5aa3abc555d59ce10890ed629dfee03d9cd
Yusef Habib <habibfernandez@gmail.com>
2017-01-12 23:54:50 +0100

    Solution to ex.19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -22,6 +22,7 @@ function getVideo() {
         })
         .catch(e => {
             // In case that the user doesn't allow to use the media devices
+            alert('Allow me to use the WEBCAAAAAAM!!!');
             console.log('Ohhh No!!!', e);
         });
 } 
@@ -38,12 +39,17 @@ function paintToCanvas() {
     return setInterval(() => {
         // From top left corner to bottom right corner
         ctx.drawImage(video, 0, 0, width, height);
-        // an special type of array for big data
         
+        // take pixels out in a special array for big data
         let pixels = ctx.getImageData(0, 0, width, height);
+
+        // mess with them
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
+        // pixels = rgbSplit(pixels);
+        pixels = greenScreen(pixels);
         // debugger;
+
+        // put pixels back
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -65,7 +71,8 @@ function takePhoto() {
     strip.insertBefore(link, strip.firstChild);
 }
 
-// Effects
+// -- EFFECTS
+
 function redEffect(pixels) {
     for(let i=0; i < pixels.data.length; i+=4) {
         pixels.data[i + 0] = pixels.data[i + 0] + 150;  // r
@@ -77,9 +84,37 @@ function redEffect(pixels) {
 
 function rgbSplit(pixels) {
     for(let i=0; i < pixels.data.length; i+=4) {
-        pixels.data[i - 150] = pixels.data[i + 0];  // r
-        pixels.data[i + 100] = pixels.data[i + 1];   // g
-        pixels.data[i - 150] = pixels.data[i + 2];  // b
+        pixels.data[i - 150] = pixels.data[i + 0];  
+        pixels.data[i + 100] = pixels.data[i + 1];  
+        pixels.data[i - 150] = pixels.data[i + 2];  
     }
     return pixels;
+}
+
+// document it properly
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+  
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
